4536f90f140c4c94401771b9465701ca28eaef0e
Hugh Rawlinson <hughr2005@gmail.com>
2022-02-06 00:12:39 +0100

    Moving enemies

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,41 @@ struct Renderable {
   bg: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+  type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+  fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+    for (_lefty, pos) in (&lefty, &mut pos).join() {
+      pos.x -= 1;
+      if pos.x < 0 {
+        pos.x = 79
+      }
+    }
+  }
+}
+
 struct State {
   ecs: World,
+}
+
+impl State {
+  fn run_systems(&mut self) {
+    let mut lw = LeftWalker {};
+    lw.run_now(&self.ecs);
+    self.ecs.maintain();
+  }
 }
 
 impl GameState for State {
   fn tick(&mut self, ctx: &mut Rltk) {
     ctx.cls();
+
+    self.run_systems();
     let positions = self.ecs.read_storage::<Position>();
     let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -41,6 +69,7 @@ fn main() -> rltk::BError {
   let mut gs = State { ecs: World::new() };
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
+  gs.ecs.register::<LeftMover>();
 
   gs.ecs
     .create_entity()
@@ -61,6 +90,7 @@ fn main() -> rltk::BError {
         fg: RGB::named(rltk::RED),
         bg: RGB::named(rltk::BLACK),
       })
+      .with(LeftMover {})
       .build();
   }
   rltk::main_loop(context, gs)
