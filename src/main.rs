328cbd2dd58f736d448d7f95fe3253cf7151a6a4
Hugh Rawlinson <hughr2005@gmail.com>
2022-02-06 12:02:45 +0100

    Add more map generation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod map;
 pub use map::*;
 mod player;
 pub use player::*;
+mod rect;
+pub use rect::Rect;
 
 pub struct State {
   ecs: World,
@@ -43,14 +45,20 @@ fn main() -> rltk::BError {
     .build()?;
 
   let mut gs = State { ecs: World::new() };
-  gs.ecs.insert(new_map_test());
   gs.ecs.register::<Player>();
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
 
+  let (rooms, map) = new_map_rooms_and_corridors();
+  gs.ecs.insert(map);
+  let (player_x, player_y) = rooms[0].center();
+
   gs.ecs
     .create_entity()
-    .with(Position { x: 40, y: 25 })
+    .with(Position {
+      x: player_x,
+      y: player_y,
+    })
     .with(Renderable {
       glyph: rltk::to_cp437('@'),
       fg: RGB::named(rltk::YELLOW),
